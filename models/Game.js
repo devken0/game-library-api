01ec64760472e221c09eda16f26f9f92b72bd5f1
devken0 <119792185+devken0@users.noreply.github.com>
2025-07-12 18:58:56 +0800

    Create game model

---
+++ models/Game.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const gameSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  genre: { type: String, required: true },
+  platform: { type: String, required: true },
+  releaseYear: { type: Number, required: true },
+  description: { type: String, default: '' }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Game', gameSchema);
