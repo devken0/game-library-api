d338f0620af1b9b146572313558c49f865893738
devken0 <119792185+devken0@users.noreply.github.com>
2025-07-14 00:19:44 +0800

    Fix routes

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/gameController');
 const auth = require('../middlewares/authMiddleware');
-const validator = require('../middlewares/validateGame');
+const { validateGame, validateGameUpdate } = require('../middlewares/validateGame');
 
-router.post('/', auth, validator, controller.createGame);
+router.post('/', auth, validateGame, controller.createGame);
 router.get('/', controller.getAllGames);
-router.put('/:id', auth, validator, controller.updateGame);
+router.put('/:id', auth, validateGameUpdate, controller.updateGame);
 router.delete('/:id', auth, controller.deleteGame);
 router.get('/search', controller.searchGame);
 
