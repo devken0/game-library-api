4da8e08bc95c5ac91570c2f587d6025483032438
devken0 <119792185+devken0@users.noreply.github.com>
2025-07-14 00:19:26 +0800

    Fix validators

--- middlewares/validateAuth.js
+++ middlewares/validateAuth.js
@@ -8,7 +8,10 @@ const validateRegister = [
   body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
   (req, res, next) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) return next(createError(400, errors.array().map(err => err.msg).join(', ')));
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map(err => err.msg).join(', ');
+      return next(createError(400, errorMessages));
+    }
     next();
   }
 ];
@@ -18,7 +21,10 @@ const validateLogin = [
   body('password').notEmpty().withMessage('Password is required'),
   (req, res, next) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) return next(createError(400, errors.array().map(err => err.msg).join(', ')));
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map(err => err.msg).join(', ');
+      return next(createError(400, errorMessages));
+    }
     next();
   }
 ];
--- middlewares/validateGame.js
+++ middlewares/validateGame.js
@@ -5,12 +5,30 @@ const validateGame = [
   body('title').notEmpty().withMessage('Game title is required'),
   body('genre').notEmpty().withMessage('Game genre is required'),
   body('platform').notEmpty().withMessage('Game platform is required'),
-  body('releaseYear').isDate().withMessage('Release year must be a number'),
+  body('releaseYear').isNumeric().withMessage('Release year must be a number'),
   (req, res, next) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) return next(createError(400, errors.array().map(err => err.msg).join(', ')));
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map(err => err.msg).join(', ');
+      return next(createError(400, errorMessages));
+    }
     next();
   }
 ];
 
-module.exports = validateGame;
+const validateGameUpdate = [
+  body('title').optional().notEmpty().withMessage('Game title cannot be empty'),
+  body('genre').optional().notEmpty().withMessage('Game genre cannot be empty'),
+  body('platform').optional().notEmpty().withMessage('Game platform cannot be empty'),
+  body('releaseYear').optional().isNumeric().withMessage('Release year must be a number'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map(err => err.msg).join(', ');
+      return next(createError(400, errorMessages));
+    }
+    next();
+  }
+];
+
+module.exports = { validateGame, validateGameUpdate };
