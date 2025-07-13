c838f21ec686dcedd0d1afe8b9b9b7a92d199e04
devken0 <119792185+devken0@users.noreply.github.com>
2025-07-13 19:52:25 +0800

    Update rateLimit message

--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
   windowMs: 60 * 1000, // time window in milliseconds. 60 * 1000 = 1 min
   max: 30, // each IP can make up to 30 requests per minute
-  message: 'Too many requests from this IP, please try again later.'
+  message: 'Too many requests, please try again later.'
 });
 
 module.exports = limiter;
